d043f374b25ca26d0512031895e58a14f31969b5
A1Zab <lisandrugrin@gmail.com>
2023-07-10 22:58:28 +0300

    .

--- src/app/store/qualities.js
+++ src/app/store/qualities.js
@@ -52,20 +52,24 @@ export const loadQualitiesList = () => async (dispatch, getState) => {
 export const getQualities = () => (state) => state.qualities.entities
 export const getQualitiesLoadingStatus = () => (state) => state.qualities.isLoading
 
-export const getQualitiesByIds = (qualitiesIds) => (state) => {
-	if (state.qualities.entities) {
-		const qualitiesArray = []
-		for (const qualId of qualitiesIds) {
-			for (const quality of state.qualities.entities) {
-				if (quality._id === qualId) {
-					qualitiesArray.push(quality)
-					break
+export const getQualitiesByIds = (qualitiesIds) =>
+	createSelector(
+		(state) => state.qualities.entities,
+		(state) => {
+			if (state) {
+				const qualitiesArray = []
+				for (const qualId of qualitiesIds) {
+					for (const quality of state) {
+						if (quality._id === qualId) {
+							qualitiesArray.push(quality)
+							break
+						}
+					}
 				}
+				return qualitiesArray
 			}
+			return []
 		}
-		return qualitiesArray
-	}
-	return []
-}
+	)
 
 export default qualitiesReducer
